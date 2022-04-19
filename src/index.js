7d9e56a6e4023c5925086062ac3c5d6c673f9983
Léo Delabre <leo.delabre@cgi.com>
2022-04-19 11:24:42 +0200

    bug fix resize phone 2

--- src/index.js
+++ src/index.js
@@ -8,12 +8,10 @@ import $ from 'jquery';
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App/>);
 
-window.addEventListener('resize', start);
 
 function start(){
     const height = $("#navigation").outerHeight();
     const heightWindow = $(window).outerHeight();
-    console.log(height)
     if(height > 0)
     {
         $("#main").css("margin-top", height);
