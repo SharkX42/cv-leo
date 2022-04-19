541565515a9180bef49f87d2aac356ed4566e869
Léo Delabre <leo.delabre@cgi.com>
2022-04-19 10:14:40 +0200

    Updates

---
+++ precache-manifest.7a6fe600629c263331391ac4489f7904.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa33612755debb317375",
+    "url": "/cv-leo/static/css/main.8b899e73.chunk.css"
+  },
+  {
+    "revision": "fa33612755debb317375",
+    "url": "/cv-leo/static/js/main.fa336127.chunk.js"
+  },
+  {
+    "revision": "30c6b463fb4d1bec8866",
+    "url": "/cv-leo/static/css/1.874b0815.chunk.css"
+  },
+  {
+    "revision": "30c6b463fb4d1bec8866",
+    "url": "/cv-leo/static/js/1.30c6b463.chunk.js"
+  },
+  {
+    "revision": "9f8f3ab36eb9e1808c77",
+    "url": "/cv-leo/static/js/runtime~main.9f8f3ab3.js"
+  },
+  {
+    "revision": "64a211b391773d2dfd1ba11e352f16b2",
+    "url": "/cv-leo/static/media/avatar.64a211b3.png"
+  },
+  {
+    "revision": "a3ce3be0adfa3096dfaa1ef0a07cc371",
+    "url": "/cv-leo/static/media/fondAbout.a3ce3be0.jpg"
+  },
+  {
+    "revision": "8f523716e33b1ce6f8f9e1e97d0b4dd9",
+    "url": "/cv-leo/index.html"
+  }
+];
