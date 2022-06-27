1fa0942fbd5054659ecde0da7d1211c80a2731a1
Léo Delabre <leo.delabre@cgi.com>
2022-06-27 12:03:36 +0200

    Updates

---
+++ precache-manifest.29f2079f64cbb1a08681956ec5c1c18c.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b5fc9b4dcf0cd4d6f82",
+    "url": "/cv-leo/static/css/main.125f100f.chunk.css"
+  },
+  {
+    "revision": "3b5fc9b4dcf0cd4d6f82",
+    "url": "/cv-leo/static/js/main.3b5fc9b4.chunk.js"
+  },
+  {
+    "revision": "c4f7f642b5f618d3248b",
+    "url": "/cv-leo/static/css/1.79e3cd17.chunk.css"
+  },
+  {
+    "revision": "c4f7f642b5f618d3248b",
+    "url": "/cv-leo/static/js/1.c4f7f642.chunk.js"
+  },
+  {
+    "revision": "9f8f3ab36eb9e1808c77",
+    "url": "/cv-leo/static/js/runtime~main.9f8f3ab3.js"
+  },
+  {
+    "revision": "814dc12d002b5baaa903e7a3afc2362c",
+    "url": "/cv-leo/static/media/avatar.814dc12d.png"
+  },
+  {
+    "revision": "f888974a0d63374cf9937df15fcd64ff",
+    "url": "/cv-leo/static/media/CV_DELABRE_LEO.f888974a.pdf"
+  },
+  {
+    "revision": "2482c36fa538ff24b63eedec971b6874",
+    "url": "/cv-leo/static/media/fondAbout.2482c36f.jpg"
+  },
+  {
+    "revision": "8666f1e2f13d8bfc348d2491b19f060d",
+    "url": "/cv-leo/static/media/bac.8666f1e2.jpg"
+  },
+  {
+    "revision": "179a595153decf5102e47a3791ea8459",
+    "url": "/cv-leo/static/media/bdd.179a5951.png"
+  },
+  {
+    "revision": "cc2736aa2bc8ecd9c4dcad1591daf735",
+    "url": "/cv-leo/static/media/cv.cc2736aa.png"
+  },
+  {
+    "revision": "c8884842ecd8fe7911ce692ce404f6ae",
+    "url": "/cv-leo/static/media/framework.c8884842.png"
+  },
+  {
+    "revision": "5b3a34b2d3ed23df14d8befc4a64439d",
+    "url": "/cv-leo/static/media/image.5b3a34b2.jpg"
+  },
+  {
+    "revision": "ca5f9ca0e2dbd25684bea78dd7d6d042",
+    "url": "/cv-leo/static/media/integration.ca5f9ca0.png"
+  },
+  {
+    "revision": "ef68079a6a2b5bd7e1aa80b0ac2d206c",
+    "url": "/cv-leo/static/media/lexi.ef68079a.png"
+  },
+  {
+    "revision": "0d89d29ba5ee0dfc75dd292532d2686e",
+    "url": "/cv-leo/static/media/nexus.0d89d29b.png"
+  },
+  {
+    "revision": "387352f73d08e423fd1d732f481db5fb",
+    "url": "/cv-leo/static/media/programmation.387352f7.png"
+  },
+  {
+    "revision": "c278f3265b232614db27173ed8aacaf1",
+    "url": "/cv-leo/static/media/soleil.c278f326.png"
+  },
+  {
+    "revision": "3c73528583289d521f305e5b3bfcad79",
+    "url": "/cv-leo/static/media/sport.3c735285.png"
+  },
+  {
+    "revision": "7c5417f647c66aef0f9f606c8da0732f",
+    "url": "/cv-leo/static/media/trip.7c5417f6.jpg"
+  },
+  {
+    "revision": "67523be9c55f22e7bb345ae06d6723df",
+    "url": "/cv-leo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/cv-leo/precache-manifest.4948077df2263d1d45e3b32647a6175c.js"
+  "/cv-leo/precache-manifest.29f2079f64cbb1a08681956ec5c1c18c.js"
 );
 
 workbox.clientsClaim();
